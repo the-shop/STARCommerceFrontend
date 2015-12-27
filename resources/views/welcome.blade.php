5d9b61d2d9058b76ee38dabbd889f19929f3d3f4
Mladen <lotar.mladen@gmail.com>
2015-12-27 18:20:23 +0100

    fix(Repo): cleanup of repository

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">STAR Commerce frontend</div>
             </div>
         </div>
     </body>
